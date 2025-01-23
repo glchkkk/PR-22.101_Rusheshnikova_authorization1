0a9eea8c192994b982dc05cb32ab227c422831eb
glchkkk <rusheshnikovaa@gmail.com>
2025-01-23 17:08:41 +0700

    Добавьте файлы проекта.

---
+++ PR-22.101_Rusheshnikova_authorization1/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace PR_22._101_Rusheshnikova_authorization1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            string login = LoginTextBox.Text.Trim();
+            string password = PasswordTextBox.Text.Trim();
+
+            using (var context = new krEntities())
+            {
+                var user = context.users.Where(a => a.login == login && a.password == password).FirstOrDefault();
+
+                if (user != null)
+                {
+                    MessageBox.Show("Добро пожаловать! Ваша роль: " + user.roles.name);
+
+                }
+            }
+        }
+    }
+}
